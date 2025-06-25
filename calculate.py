eca7a7c43b3e24e39e0d031c7cb18a9c9c63e3cc
mohamedjuma367 <mohamedjuma5541@gmail.com>
2025-06-25 13:39:02 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -1,8 +1,6 @@
 import streamlit as st
-
 x = st.number_input("Enter first number")
 y = st.number_input("Enter second number")
-
 add = st.write(x+y)
 subtract = st.write(x-y)
 multiply = st.write(x*y)
